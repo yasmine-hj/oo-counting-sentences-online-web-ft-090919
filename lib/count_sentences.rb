f29c48288726767553946073e8a385487ac0e492
Yasmine Hajaji <yasminehajaji95@gmail.com>
2019-09-17 20:53:00 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,14 +3,14 @@ require 'pry'
 class String
 
   def sentence?
-    if self.end_with? (".")
+    if self.end_with?(".")
       true
     else
       false
     end
   end
 
-    if self.end_with? ("?")
+    if self.end_with?("?")
       true
     else
       false
@@ -18,7 +18,7 @@ class String
   end
 
   def exclamation?
-    if self.end_with? ("!")
+    if self.end_with?("!")
       true
     else
       false
